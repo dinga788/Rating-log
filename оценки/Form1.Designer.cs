ec512be7bb06c02bd09d5d68d0223d675c662435
dinga788 <iliasadowsky2005@gmail.com>
2024-11-06 20:02:54 +0300

    first commit

---
+++ оценки/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace оценки
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.Группа = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Студент = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Средний_бал = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 49);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(802, 426);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Группа,
+            this.Студент,
+            this.Средний_бал});
+            this.dataGridView2.Location = new System.Drawing.Point(820, 49);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(428, 426);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // Группа
+            // 
+            this.Группа.HeaderText = "Группа";
+            this.Группа.MinimumWidth = 6;
+            this.Группа.Name = "Группа";
+            this.Группа.Width = 125;
+            // 
+            // Студент
+            // 
+            this.Студент.HeaderText = "Студент";
+            this.Студент.MinimumWidth = 6;
+            this.Студент.Name = "Студент";
+            this.Студент.Width = 125;
+            // 
+            // Средний_бал
+            // 
+            this.Средний_бал.HeaderText = "Средний_бал";
+            this.Средний_бал.MinimumWidth = 6;
+            this.Средний_бал.Name = "Средний_бал";
+            this.Средний_бал.Width = 125;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(367, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Журнал";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(1003, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Рейтинг";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1296, 577);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Группа;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Студент;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Средний_бал;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ оценки/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace оценки
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView1.CellValueChanged += DataGridView1_CellValueChanged;
+            dataGridView1.CellValidating += DataGridView1_CellValidating;
+            FillDataGridViewWithStudents();
+            TransferAndSortData();
+        }
+
+        private void DataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == dataGridView1.Columns["Химия"].Index ||
+                e.ColumnIndex == dataGridView1.Columns["Физика"].Index ||
+                e.ColumnIndex == dataGridView1.Columns["Математика"].Index)
+            {
+                var rowIndex = e.RowIndex;
+
+                CalculateAverage(rowIndex);
+            }
+        }
+
+        private void TransferAndSortData()
+        {
+            dataGridView2.Rows.Clear();
+
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (!row.IsNewRow)
+                {
+                    dataGridView2.Rows.Add(
+                        row.Cells["Группа"].Value,
+                        row.Cells["Студент"].Value,
+                        row.Cells["Средний_бал"].Value
+                    );
+                }
+            }
+
+            SortDataGridViewByAverageScore(dataGridView2);
+        }
+
+        private void SortDataGridViewByAverageScore(DataGridView dgv)
+        {
+            dgv.Sort(dgv.Columns["Средний_бал"], ListSortDirection.Descending);
+        }
+
+        private void CalculateAverage(int rowIndex)
+        {
+            var currentRow = dataGridView1.Rows[rowIndex];
+
+            double? химия = (double?)currentRow.Cells["Химия"].Value;
+            double? физика = (double?)currentRow.Cells["Физика"].Value;
+            double? математика = (double?)currentRow.Cells["Математика"].Value;
+
+            int count = 0;
+            double sum = 0;
+
+            if (химия.HasValue && химия != 0)
+            {
+                sum += химия.Value;
+                count++;
+            }
+
+            if (физика.HasValue && физика != 0)
+            {
+                sum += физика.Value;
+                count++;
+            }
+
+            if (математика.HasValue && математика != 0)
+            {
+                sum += математика.Value;
+                count++;
+            }
+
+            double? average = count > 0 ? (double?)sum / count : null;
+
+            currentRow.Cells["Средний_бал"].Value = average;
+
+            TransferAndSortData();
+        }
+
+        private void DataGridView1_CellValidating(object sender, DataGridViewCellValidatingEventArgs e)
+        {
+            if (e.ColumnIndex == dataGridView1.Columns["Химия"].Index ||
+                e.ColumnIndex == dataGridView1.Columns["Физика"].Index ||
+                e.ColumnIndex == dataGridView1.Columns["Математика"].Index)
+            {
+                int value;
+                if (int.TryParse(e.FormattedValue.ToString(), out value))
+                {
+                    if (value < 2 || value > 5)
+                    {
+                        MessageBox.Show("Допустимы значения от 2 до 5.");
+                        e.Cancel = true;
+                        dataGridView1.CurrentCell.Value = DBNull.Value;
+                        dataGridView1.Rows[e.RowIndex].ErrorText = "";
+                        dataGridView1.CancelEdit();
+                    }
+                }
+                else
+                {
+                    e.Cancel = false;
+                }
+            }
+        }
+
+        private void FillDataGridViewWithStudents()
+        {
+            Student[] students = new Student[]
+            {
+                new Student { Группа = "Группа 1", Студент = "Иван Иванов", Химия = null, Физика = null, Математика = null, Средний_бал = null },
+                new Student { Группа = "Группа 1", Студент = "Петр Петров", Химия = null, Физика = null, Математика = null, Средний_бал = null },
+                new Student { Группа = "Группа 1", Студент = "Сергей Сергеев", Химия = null, Физика = null, Математика = null, Средний_бал = null },
+                new Student { Группа = "Группа 1", Студент = "Алексей Алексеев", Химия = null, Физика = null, Математика = null, Средний_бал = null },
+                new Student { Группа = "Группа 1", Студент = "Дмитрий Дмитриев", Химия = null, Физика = null, Математика = null, Средний_бал = null },
+                new Student { Группа = "Группа 1", Студент = "Андрей Андреев", Химия = null, Физика = null, Математика = null, Средний_бал = null },
+                new Student { Группа = "Группа 1", Студент = "Михаил Михайлов", Химия = null, Физика = null, Математика = null, Средний_бал = null },
+                new Student { Группа = "Группа 1", Студент = "Николай Николаев", Химия = null, Физика = null, Математика = null, Средний_бал = null },
+
+                new Student { Группа = "Группа 2", Студент = "Анна Васильева", Химия = null, Физика = null, Математика = null, Средний_бал = null },
+                new Student { Группа = "Группа 2", Студент = "Марина Смирнова", Химия = null, Физика = null, Математика = null, Средний_бал = null },
+                new Student { Группа = "Группа 2", Студент = "Елена Егорова", Химия = null, Физика = null, Математика = null, Средний_бал = null },
+                new Student { Группа = "Группа 2", Студент = "Ольга Орлова", Химия = null, Физика = null, Математика = null, Средний_бал = null },
+                new Student { Группа = "Группа 2", Студент = "Татьяна Тихонова", Химия = null, Физика = null, Математика = null, Средний_бал = null },
+                new Student { Группа = "Группа 2", Студент = "Светлана Светлова", Химия = null, Физика = null, Математика = null, Средний_бал = null },
+                new Student { Группа = "Группа 2", Студент = "Кристина Крылова", Химия = null, Физика = null, Математика = null, Средний_бал = null },
+                new Student { Группа = "Группа 2", Студент = "Дарья Дмитриева", Химия = null, Физика = null, Математика = null, Средний_бал = null },
+
+                new Student { Группа = "Группа 3", Студент = "Александр Александров", Химия = null, Физика = null, Математика = null, Средний_бал = null },
+                new Student { Группа = "Группа 3", Студент = "Виктор Викторов", Химия = null, Физика = null, Математика = null, Средний_бал = null },
+                new Student { Группа = "Группа 3", Студент = "Евгений Евгеньев", Химия = null, Физика = null, Математика = null, Средний_бал = null },
+                new Student { Группа = "Группа 3", Студент = "Константин Константинов", Химия = null, Физика = null, Математика = null, Средний_бал = null },
+                new Student { Группа = "Группа 3", Студент = "Максим Максимов", Химия = null, Физика = null, Математика = null, Средний_бал = null },
+                new Student { Группа = "Группа 3", Студент = "Павел Павлов", Химия = null, Физика = null, Математика = null, Средний_бал = null },
+                new Student { Группа = "Группа 3", Студент = "Роман Романов", Химия = null, Физика = null, Математика = null, Средний_бал = null },
+                new Student { Группа = "Группа 3", Студент = "Ярослав Ярославов", Химия = null, Физика = null, Математика = null, Средний_бал = null },
+            };
+
+            dataGridView1.DataSource = students;
+            dataGridView1.AutoGenerateColumns = true;
+
+            foreach (DataGridViewColumn column in dataGridView1.Columns)
+            {
+                if (column.Name == "Группа" || column.Name == "Студент" || column.Name == "Средний_бал")
+                {
+                    column.ReadOnly = true;
+                }
+            }
+
+            dataGridView1.Refresh();
+
+            foreach (DataGridViewColumn column in dataGridView2.Columns)
+            {
+                if (column.Name == "Группа" || column.Name == "Студент" || column.Name == "Средний_бал")
+                {
+                    column.ReadOnly = true;
+                }
+                else
+                {
+                    column.Visible = false;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+    public class Student
+    {
+        public string Группа { get; set; }
+        public string Студент { get; set; }
+        public double? Химия { get; set; }
+        public double? Физика { get; set; }
+        public double? Математика { get; set; }
+        public double? Средний_бал { get; set; }
+    }
+}
